35a226ad337c7c024243986859932cbc3ed37260
flashgrinder <flashgrinder92@gmail.com>
2023-06-23 12:15:57 +0400

    some fix fancybox

--- src/libs/modules/fancybox.js
+++ src/libs/modules/fancybox.js
@@ -2,8 +2,7 @@ import  { Fancybox } from '@fancyapps/ui';
 
 function init() {
 
-    Fancybox.bind("[data-fancybox='photo-blog']", {
-        selector: '.js-fancybox-init .swiper-slide:not(.swiper-slide-duplicate)',
+    Fancybox.bind("[data-fancybox]", {
         Thumbs: {
             type: "classic",
         },
